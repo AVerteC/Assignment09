c24debae656763cacd719e4d94995a810a8db8e3
AVerteC <alan.r.chen@gmail.com>
2025-05-17 20:01:16 -0700

    Finished step 4 of strings excercise

---
+++ js/strings.js
@@ -0,0 +1,41 @@
+//STEP 1
+// let name = prompt('Enter your name:')
+// if (name === null || name.length === 0) {
+//     alert('Invalid name input')
+// }
+// alert(`The length of your name is: ${name.length}`)
+//STEP 2
+// let name = prompt('Enter your name: ')
+// if (name === null || name.length === 0) {
+//     alert('Invalid name input')
+// }
+
+// let number = parseInt(prompt('Specify the index you want to find a letter at: '))
+// if (isNaN(number) || number < 0 || number > name.length - 1) {
+//     alert('You have entered an invalid index.')
+// } else {
+//     alert(`The character at index ${number} is: ${name.charAt(number)}`)
+// }
+//STEP 3
+// let firstName = prompt('Enter your first name:')
+// let lastName = prompt('Enter your last name:')
+// alert(`Your name is: ${firstName + ' ' + lastName}`)
+//STEP 4
+// let phrase = "The quick brown fox jumps over the lazy dog"
+// const regex = /(fox)/
+// const result = regex.exec(phrase)
+// alert(`'fox' starts at the index: ${result.index}`)
+//STEP 5
+let phrase = "The quick brown fox jumps over the lazy dog"
+const searchKey = 'fox'
+const result = phrase.lastIndexOf(searchKey)
+alert(`the last 'fox' starts at the index: ${result}`)
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
