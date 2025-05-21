c46c5e6a8f787c8eb44530862a635e849ace5360
AVerteC <alan.r.chen@gmail.com>
2025-05-20 18:28:13 -0700

    Finished dates

--- js/dates.js
+++ js/dates.js
@@ -1,9 +1,132 @@
 //STEP 1
+// const months = {
+//     "january": 0,
+//     "february": 1,
+//     "march": 2,
+//     "april": 3,
+//     "may": 4,
+//     "june": 5,
+//     "july": 6,
+//     "august": 7,
+//     "september": 8,
+//     "october": 9,
+//     "november": 10,
+//     "december": 11
+// }
+// let month = prompt('Enter the month: ')
+// if (month === null || month.length === 0) {
+//     let temp = 'Invalid month input.'
+//     alert(temp)
+//     throw new Error(temp)
+// } else if (months[month.trim().toLowerCase()] === undefined) {
+//     let temp = 'Invalid month input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
 
-//STEP 2
+// monthNumber = months[month.trim().toLowerCase()]
+// let year = parseInt(prompt('Enter the year: '))
+// if (isNaN(year) || year < 0) {
+//     let temp = 'Invalid year input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
+// year = Number(year)
 
+// const daysInMonth = new Date(year, monthNumber, 0).getDate()
+// let temp = month.charAt(0).toUpperCase() + month.substring(1)
+// console.log(temp)
+// console.log(`There are ${daysInMonth} days in ${month}, ${year}.`)
+// //STEP 2
+// let date = prompt("Enter the date in YYYY-MM-DD format: ")
+// if (date === null || date.length === 0) {
+//     let temp = 'Invalid date input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
+// dateNums = date.split('-')
+// for (let i = 0; i < 3; i++) {
+//     dateNums[i] = Number(dateNums[i])
+//     if (isNaN(dateNums[i])) {
+//         let temp = 'Invalid date input.'
+//         alert(temp)
+//         throw new Error(temp)
+//     }
+// }
+// if (dateNums.length !== 3 || dateNums[1] === 0 || dateNums[2] === 0) {
+//     let temp = 'Invalid date input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
+
+
+// dateObj = new Date(date[0], (date[1] - 1), date[2]).getMonth()
+// const months = {
+//     0: "January",
+//     1: "February",
+//     2: "March",
+//     3: "April",
+//     4: "May",
+//     5: "June",
+//     6: "July",
+//     7: "August",
+//     8: "September",
+//     9: "October",
+//     10: "November",
+//     11: "December"
+// }
+// console.log(`The month for the date ${date} is ${months[dateObj]}.`)
 //STEP 3
-
+// let date = prompt("Enter the date in YYYY-MM-DD format: ")
+// if (date === null || date.length === 0) {
+//     let temp = 'Invalid date input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
+// dateNums = date.split('-')
+// for (let i = 0; i < 3; i++) {
+//     dateNums[i] = Number(dateNums[i])
+//     if (isNaN(dateNums[i])) {
+//         let temp = 'Invalid date input.'
+//         alert(temp)
+//         throw new Error(temp)
+//     }
+// }
+// if (dateNums.length !== 3 || dateNums[1] === 0 || dateNums[2] === 0) {
+//     let temp = 'Invalid date input.'
+//     alert(temp)
+//     throw new Error(temp)
+// }
+// dateObj = new Date(date[0], (date[1] - 1), date[2]).getDay()
+// if (dateObj === 0 || dateObj === 6) {
+//     console.log(`The date ${date} falls on a weekend.`)
+// }
 //STEP 4
+// date = new Date()
+// date.setDate(date.getDate() - 1)
+
+// const daysOfWeek = {
+//     0: "Sunday",
+//     1: "Monday",
+//     2: "Tuesday",
+//     3: "Wednesday",
+//     4: "Thursday",
+//     5: "Friday",
+//     6: "Saturday"
+// }
+// yesterday = date.getDay()
+// console.log(`Yesterday was ${daysOfWeek[yesterday]}.`)
 
 //STEP 5
+// date = new Date()
+// const daysOfWeek = {
+//     0: "Sunday",
+//     1: "Monday",
+//     2: "Tuesday",
+//     3: "Wednesday",
+//     4: "Thursday",
+//     5: "Friday",
+//     6: "Saturday"
+// }
+// currentDay = date.getDay()
+// console.log(`Today is ${daysOfWeek[currentDay].charAt(0).toUpperCase()}.`)
